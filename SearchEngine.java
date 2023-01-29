14e29044145dac2bc805fdfea995dfb657b53abb
jut008 <122562428+jut008@users.noreply.github.com>
2023-01-29 11:05:05 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -5,20 +5,34 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+    int arrayCounter = 0;
+    int storage = 10;
+    String[] strArray = new String[storage];
+    String[] containsArray = new String[storage];
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Judy's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
+            return ("Strings stored: [" + strArray + "]");
+        } 
+        else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    for (int i = 0 ; i < strArray.length ; i++) {
+                        if (strArray[i].contains(parameters[1])) {
+                            containsArray[i] = strArray[i];
+                        }
+                    }
+                }
+            return ("Found: " + containsArray);
+            } 
+            else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    strArray[arrayCounter] = parameters[1];
+                    arrayCounter++;
+                    return (parameters[1] + " stored! Strings stored: [" + strArray + "]");
                 }
             }
             return "404 Not Found!";
